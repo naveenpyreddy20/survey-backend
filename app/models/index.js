af983c8171e20a886d81bce32dc26550d2a90ef8
naveenpyreddy20 <naveenpyreddy@gmail.com>
2022-07-16 08:09:34 -0500

    done

--- app/models/index.js
+++ app/models/index.js
@@ -25,6 +25,31 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.user = require("../models/userModel")(sequelize, Sequelize);
+db.survey = require("../models/surveyModel")(sequelize, Sequelize);
+db.question = require("../models/questionModel")(sequelize, Sequelize);
+db.option = require("../models/optionsModel")(sequelize, Sequelize);
+
+//relatinons
+db.user.hasMany(db.survey, {
+    as: 'Survey'
+  });
+db.survey.belongsTo(db.user, {
+    foreignKey: 'userId', as: 'User',
+  });
+
+  db.survey.hasMany(db.question, {
+    as: 'Question'
+  });
+  db.question.belongsTo(db.survey, {
+    foreignKey: 'surveyId', as: 'Survey',
+  });
+  
+  db.question.hasMany(db.option, {
+    as: 'Option'
+  });
+  db.option.belongsTo(db.question, {
+    foreignKey: 'questionId', as: 'Question',
+  });
 
 
 module.exports = db;
